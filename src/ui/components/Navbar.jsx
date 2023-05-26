0ab539db2e776195fb8c5cf8ed2c3db22e909eac
Juan Gustin <juandgustin@gmail.com>
2023-05-26 18:22:59 -0500

    Nav Bar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -9,21 +9,30 @@ export const Navbar = () => {
 
       <div className='navbar-collapse'>
         <div className='navbar-nav'>
-          <NavLink className='nav-item nav-link' to='/marvel'>
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? 'active' : ''}`
+            }
+            to='/marvel'
+          >
             Marvel
           </NavLink>
 
-          <NavLink className='nav-item nav-link' to='/dc'>
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? 'active' : ''}`
+            }
+            to='/dc'
+          >
             DC
           </NavLink>
         </div>
       </div>
 
-      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
+      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
-          <NavLink className='nav-item nav-link' to='/login'>
-            Logout
-          </NavLink>
+          <span className='nav-item nav-link text-primary'>GustinDev</span>
+          <button className='nav-item nav-link btn'>Logout</button>
         </ul>
       </div>
     </nav>
